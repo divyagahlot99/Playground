e8f4ecec3a840caf380cd9e0be43b9fc3ac54ed3
Divya Gahlot <48877568+divyagahlot99@users.noreply.github.com>
2020-05-11 21:00:08 +0530

    Test code pushed to repository.

---
+++ CGPA/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+ 	public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+      	float a = sc.nextFloat();
+      	System.out.println(a);
+    } 
+}
